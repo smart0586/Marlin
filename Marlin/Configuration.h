091eb035bce7871805583b18768d8fce88b1e6e3
Sergio Martinez <smart0586@gmail.com>
2021-01-06 17:11:12 -0800

    Updating Author

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -72,7 +72,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(SM, 09-16-2020, Ender-3 Pro)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(SM, 6-Jan-21, E3Pro)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
