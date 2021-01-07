e8f9cf9daae7d43f2583a6e75926177d7e1d1a3e
Sergio Martinez <smart0586@gmail.com>
2021-01-06 17:12:20 -0800

    Updated Machine Name

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -134,7 +134,7 @@
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
-#define CUSTOM_MACHINE_NAME "Marlin 2.0.6 Ender-3 Pro BTT SKR Mini & BLTOUCH"
+#define CUSTOM_MACHINE_NAME "Marlin 2.0.8 Bugfix"
 
 // Printer's unique ID, used by some programs to differentiate between machines.
 // Choose your own or use a service like https://www.uuidgenerator.net/version4
